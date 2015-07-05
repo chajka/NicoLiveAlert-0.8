cd57233f533d9669f103fe988b79949d11cf41cf
Чайка <mellanie335@gmail.com>
2015-07-06 05:52:57 +0900

    Add empty class NLUser into project.

---
+++ NicoLiveAlert/User Management/NLUser.h
@@ -0,0 +1,13 @@
+//
+//  NLUser.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/6/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLUser : NSObject
+
+@end
---
+++ NicoLiveAlert/User Management/NLUser.m
@@ -0,0 +1,13 @@
+//
+//  NLUser.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/6/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "NLUser.h"
+
+@implementation NLUser
+
+@end
