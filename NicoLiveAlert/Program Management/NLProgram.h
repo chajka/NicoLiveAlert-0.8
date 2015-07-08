7bec3d3b14d58882a959064eb026dec52f131b50
Чайка <mellanie335@gmail.com>
2015-07-08 13:00:43 +0900

    Add class NLProgram

---
+++ NicoLiveAlert/Program Management/NLProgram.h
@@ -0,0 +1,16 @@
+//
+//  NLProgram.h
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/8/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NLProgram : NSObject {
+	NSString							*liveNumber;
+	
+}
+
+@end
---
+++ NicoLiveAlert/Program Management/NLProgram.m
@@ -0,0 +1,23 @@
+//
+//  NLProgram.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/8/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "NLProgram.h"
+
+@implementation NLProgram
+#pragma mark - synthesize properties
+#pragma mark - class method
+#pragma mark - constructor / destructor
+#pragma mark - override
+#pragma mark - delegate
+#pragma mark - properties
+#pragma mark - actions
+#pragma mark - messages
+#pragma mark - private
+#pragma mark - C functions
+
+@end
