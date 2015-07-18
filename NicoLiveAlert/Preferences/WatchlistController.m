3e61a83bfa1d7a62096e10ecc281c055801ad51b
Чайка <mellanie335@gmail.com>
2015-07-18 16:42:19 +0900

    Add watch list controller.

---
+++ NicoLiveAlert/Preferences/WatchlistController.m
@@ -0,0 +1,22 @@
+//
+//  WatchlistController.m
+//  NicoLiveAlert
+//
+//  Created by Чайка on 7/18/15.
+//  Copyright (c) 2015 Instrumentality of Mankind. All rights reserved.
+//
+
+#import "WatchlistController.h"
+
+@interface WatchlistController ()
+
+@end
+
+@implementation WatchlistController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do view setup here.
+}
+
+@end
